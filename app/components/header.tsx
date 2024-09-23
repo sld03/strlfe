a996a773303587fc68da90b1cd2d81337c23cacc
sld03 <sonld.uet@gmail.com>
2024-09-23 16:11:05 +0700

    add some font and modify the background

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+      <header>
+        <nav>
+          {/* Your navigation links, logo, etc. */}
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </nav>
+      </header>
+    );
+  }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import {Header, Footer} from "./components"
+
+const magnifico = localFont({
+  src: './fonts/Magnifico.ttf',  
+  variable: '--font-magnifico',   
+  weight: '400',                    
+  style: 'normal',                  
+  display: 'swap',                  
+});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +34,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${magnifico.variable} ${geistMono.variable} antialiased`}>
+        <Header></Header>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
